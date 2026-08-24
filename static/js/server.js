090b79c39bafd021ca8a132156b0e62234f6f0a4
Yagoodev <yago.yaguinho@live.com>
2026-08-24 20:49:19 -0300

    feat: estblishing a connection

---
+++ static/js/server.js
@@ -0,0 +1,6 @@
+(() => {
+  const ws = new WebSocket('ws://localhost:8080/ws');
+  ws.onopen = () => console.log('Conexão WebSocket estabelecida.')
+  ws.onclose   = e => console.log("fechou", e.code);
+  ws.onmessage = e => console.log("recebi:", e.data);
+})()
